afb59f7a647d91694795313b63d0287f1d55c308
Raphael Jacob <r.jacob2002@gmail.com>
2026-04-16 20:40:31 +0000

    add volumes script

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,6 @@
 FROM docker:latest AS base
 RUN apk add --no-cache jq borgbackup openssh-client
+
+FROM base as volumes
+COPY volumes.sh /volumes.sh
+CMD ["/volumes.sh"]
---
+++ volumes.sh
@@ -0,0 +1,7 @@
+for mount in $mounts; do
+    source=$(echo "$mount" | jq -r '.Source')
+    destination=$(echo "$mount" | jq -r '.Destination')
+    echo " - Volume $source:$destination"
+    echo $container_mounts_borg_prefix$destination
+#    borg create ::$(echo $container_mounts_borg_prefix$destination | sed 's/:/::/g' | sed 's/\//:/g'):$(date -Iseconds) /host$source
+done
