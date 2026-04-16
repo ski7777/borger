70fb323cc08600468570bb68eac12efed56ca794
Raphael Jacob <r.jacob2002@gmail.com>
2026-04-16 20:44:22 +0000

    echo file count

--- volumes.sh
+++ volumes.sh
@@ -3,5 +3,6 @@ for mount in $mounts; do
     destination=$(echo "$mount" | jq -r '.Destination')
     echo " - Volume $source:$destination"
     echo $container_mounts_borg_prefix$destination
+    echo $(find $source | wc -l)
 #    borg create ::$(echo $container_mounts_borg_prefix$destination | sed 's/:/::/g' | sed 's/\//:/g'):$(date -Iseconds) /host$source
 done
