2d423c055b7255a50a7536f48d5a2bd6b3a418ae
Raphael Jacob <r.jacob2002@gmail.com>
2026-04-16 21:03:26 +0000

    volumes: mounts as base64

--- volumes.sh
+++ volumes.sh
@@ -1,4 +1,4 @@
-for mount in $mounts; do
+for mount in $(echo $mounts | base64 -d); do
     source=$(echo "$mount" | jq -r '.Source')
     destination=$(echo "$mount" | jq -r '.Destination')
     echo " - Volume $source:$destination"
