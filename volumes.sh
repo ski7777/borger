3022269bda94cf95a79191b1f002bb9adc07b079
Raphael Jacob <r.jacob2002@gmail.com>
2026-07-21 15:10:07 +0000

    next

--- volumes.sh
+++ volumes.sh
@@ -4,7 +4,5 @@ for mount in $(echo $mounts | base64 -d | jq -c '.[]'); do
     destination=$(echo "$mount" | jq -r '.Destination')
     echo " - Volume $source:$destination"
     echo $container_mounts_borg_prefix$destination
-    echo /host$source
-    ls 
     borg create ::$(echo $container_mounts_borg_prefix$destination | sed 's/:/::/g' | sed 's/\//:/g'):$(date -Iseconds) $destination
 done
